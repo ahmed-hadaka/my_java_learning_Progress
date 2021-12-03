c9e696c65fcd007c70c85a9a34a2776f59cb2fd5
AhmedHadaka <hmdhadaka@gmail.com>
2021-12-03 15:59:46 +0200

    apply new feature

--- programming 1/calc_avg_and_sum.java
+++ programming 1/calc_avg_and_sum.java
@@ -14,8 +14,8 @@ public class calc_avg_and_sum{
         double sum = 0;
         float num = 1;
         double avg;
-        int x = 0, flag = 0;
-        char answer;
+        int x = 0, flag1 = 0,flag2=0;
+        String answer;
         System.out.println("enter ur numbers to sum , once U finished or wan't leave...press 0 ");
         num = sc.nextFloat();
         if (num != 0) {
@@ -24,55 +24,28 @@ public class calc_avg_and_sum{
                 num = sc.nextFloat();
             }
             System.out.println("the sum is : " + sum);
-            flag = 1;
+            flag1 = 1;
         }
         System.out.println("thanks for Ur time");
-        if (flag == 1) {
-            System.out.println("do U want the avarege :  y/ n ?");
-            answer = sc.next().charAt(0);
-
-            switch (answer)
-             {
-            case 'y':
-            case 'Y':
+        if (flag1 == 1) {
+            do{
+            System.out.println("do U want the avarege :  yas/ no ?");
+            answer = sc.next();
+            if (answer.equalsIgnoreCase("yas")) {
                 avg = sum / x;
                 System.out.println("the avarege is" + avg);
-                break;
-
-            case 'n':
-            case 'N':
+                System.out.println("FINISHED");
+                flag2 = 0;
+            }
+            else if (answer.equalsIgnoreCase("no")) {
                 System.out.println("thanks for Ur time");
-                break;
-            default:
-
-                while (answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N') {
-                    System.out.println("unvaliad choice");
-                    answer = sc.next().charAt(0);
-                }  
-       
-            if (answer == 'y' || answer == 'Y' || answer == 'n' || answer == 'N') {
-                    
-                switch (answer) {
-                    case 'y':
-                    case 'Y':
-                        avg = sum / x;
-                        System.out.println("the avarege is" + avg);
-                break;
-                    case 'n':
-                    case 'N':
-                        System.out.println("thanks for Ur time");
-
-                break;
-                    default:
-                        System.out.println("");
-
-                }
+                flag2 = 0;
             }
-    
-
-                
-
-            }
+            else {
+                flag2=1;
+                System.out.println("unvaliad choice");
+            } 
+        } while (flag2 == 1);
 
         }
    }
