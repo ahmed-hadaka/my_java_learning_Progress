67e79a6ac24e0c27731227f0383314b306f59c1a
AhmedHadaka <hmdhadaka@gmail.com>
2021-12-06 07:33:05 +0200

    update

--- programming 2 with  OOP/Employee_class.java
+++ programming 2 with  OOP/Employee_class.java
@@ -5,41 +5,52 @@ public class Employee_class {
     private double Salary;
     private double Bonus;
     private boolean Resident;
-public Employee_class(){
+    private static int num_of_objects;
+public Employee_class(){           //constructor 1 with overloading
     Emp_ID=100;
     Name="Ahmed";
     Depart="not assigned";
     Salary=5000;
     Bonus=600;
-    Resident=true;
+    Resident = true;
+    num_of_objects++;
 }
 
-public Employee_class(int id, String n) {
+public Employee_class(int id, String n) {      //constructor 2 with overloading
     Emp_ID = id;
     Name = n;
+    num_of_objects++;
 }
 
-public Employee_class(int id, String n, boolean res) {
-    this(id, n);
+public Employee_class(int id, String n, boolean res) {         //constructor 3 with overloading and constructor chaining
+    this(id, n);  
     Resident = res;
+    num_of_objects++;
 }
 
-public Employee_class(int id, String n, String dep, double s, double b, boolean res) {
+public Employee_class(int id, String n, String dep, double s, double b, boolean res) {       //constructor 4 with overloading and constructor chaining
     this(id, n, res);
     Depart = dep;
     Salary = s;
     Bonus = b;
+    num_of_objects++;
 }
-public void set_salary(double s){
+
+public static int get_num_of_objects() {    //static method
+    return num_of_objects;
+}
+
+
+public void set_salary(double s){          //method with overloading
     Salary = s;
 }  
 
-public void set_salary(double s, double b) {
+public void set_salary(double s, double b) {         //method with overloading
     this.set_salary(s);
     Bonus = b;
 }
 
-public void depart(String d) {
+public void set_depart(String d) {
     Depart=d;
 }
 
--- programming 2 with  OOP/Employee_class_call.java
+++ programming 2 with  OOP/Employee_class_call.java
@@ -1,7 +1,14 @@
 public class Employee_class_call {
   public static void main(String[] args) {
-      Employee_class Emp1 = new Employee_class();
-      Emp1.print_Emp_data(); 
+    Employee_class Emp1 = new Employee_class();
+    Employee_class Emp2 = new Employee_class();
+    Employee_class Emp3 = new Employee_class();
+
+    Emp1.set_salary(13000);
+    Emp1.print_Emp_data();
+    System.out.println(Employee_class.get_num_of_objects());  //calling static method
+    // System.out.println(Emp1.get_num_of_objects());
+   
     }
     
 }
