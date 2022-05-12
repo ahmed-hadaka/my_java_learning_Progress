1da45b4e8420cce8dd96c3c2b22906c38947e275
AhmedHadaka <hmdhadaka@gmail.com>
2022-05-12 14:52:02 +0200

    new dataStructure

---
+++ DataStructures_and_Algorithms/Graphs/Demonstrates_Shortest_Path/Distance_Parent.java
@@ -0,0 +1,11 @@
+package Demonstrates_Shortest_Path;
+
+public class Distance_Parent {
+    public int distance;
+    public int parentVert;
+
+    public Distance_Parent(int pv, int d) {
+        distance = d;
+        parentVert = pv;
+    }
+}
---
+++ DataStructures_and_Algorithms/Graphs/Topological_Sorting/Vertex.java
@@ -0,0 +1,13 @@
+package Topological_Sorting;
+
+public class Vertex {
+    private final char label;
+
+    public Vertex(char label){
+        this.label = label;
+    }
+
+    public char getLabel() {
+        return label;
+    }
+}
