f4ca6f8ca4930e2eabfe98de19496e1957892b4e
AhmedHadaka <hmdhadaka@gmail.com>
2022-12-11 21:08:22 +0200

    clean repo

--- Console_Projects/Employee_System/Displayable.java
+++
@@ -1,9 +0,0 @@
-public interface Displayable {
-    String companyName = "Value";
-    String fax = "Example pl pla pla";
-
-    public void displayAllDetails();
-
-    public void displayEarning();
-
-}
